f67b73d40b10cd9ad80820117466d8767e344161
Andy Williams <andywilliams682@gmail.com>
2016-11-04 10:43:47 -0700

    Update UnsortedFunctions.py
    Just changed some comments to explain the weird file. It is likely that steady_check is not compatible with the new control volume scripts. I haven't touched it in a while because it's pretty much not useful right now.

--- Chemistry/MaterialsBalance/UnsortedFunctions.py
+++ Chemistry/MaterialsBalance/UnsortedFunctions.py
@@ -1,10 +1,5 @@
-# This library entails a plethora of functions that lead to the solving of a multi-unit process materials balance
-# problem. It can perform a degree of freedom analysis, setup equations and control volume, solve equations and control
-# volumes, determine the average molar mass of a system, and identify if a system is in steady state.
-#
-# Next implementations:
-# PyQt UI to avoid ALL THESE INPUTS
-# The ability to handle chemical reactions within a unit process
+# This has two functions that I didn't really feel like putting into it's own fancy 
+# category yet. They will get one eventually
 
 import periodictable as ptable
 import sympy as sp
@@ -31,7 +26,6 @@ def det_avg_molar_mass(fraction_type, fractions_dict):
 
 
 # This function uses the control volume setup to determine if there is any accumulation in the system.
-# IT IS NOT FINISHED
 def steady_check(control_volume_dimensions):
     mat_in = 0
     mat_out = 0
